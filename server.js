8e2c251323094f1decac8d68f7b4cc76a7f11a7f
Janette King <janetteking@proton.me>
2024-10-07 21:49:09 -0400

    installed cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
@@ -9,6 +10,11 @@ const PORT = process.env.PORT || 4000
 const app = express()
 
 //middleware
+app.use(
+  cors({
+    origin: '*',
+  })
+)
 app.use(express.json())
 
 app.use((req, res, next) => {
